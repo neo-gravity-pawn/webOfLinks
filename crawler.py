dd3adcca5338c77063c99a5b2af7782047f6c6e2
Neo Gravity <neo_gravity_pawn@outlook.com>
2021-03-28 16:00:46 +0200

    Add first crawl functionality and test

--- crawler.py
+++ crawler.py
@@ -1,9 +1,34 @@
+from bs4 import BeautifulSoup
+from urllib import request, parse
+
 class Crawler:
+    def __init__(self):
+        self.results = dict()
+
     def set_start_url(self, url):
         self.start_url = url
     
     def run(self):
-        pass
+        response = request.urlopen(self.start_url)
+        self.__parse_for_links(self.start_url, response)
 
     def get_results(self):
-        return 'nix'
+        return self.results
+    
+    def __parse_for_links(self, url, response):
+        soup = BeautifulSoup(response, 'html.parser')
+        status = response.status
+        for link in soup.find_all('a', href=True):
+            self.__process_link(url, link['href'])
+
+    def __process_link(self, base_url, url):
+        is_external = url.startswith('http')
+        if not is_external:
+            url = self.__convert_to_absolute_url(base_url, url)
+        already_discoverd = url in self.results
+        if not already_discoverd:
+            self.results[url] = {"from": base_url, "to": url, "is_external": is_external, "status": 0}
+        return self.results[url]
+    
+    def __convert_to_absolute_url(self, base_url, url):
+        return parse.urljoin(base_url, url) if not url.startswith(base_url) else url
--- crawler_test.py
+++ crawler_test.py
@@ -1,19 +1,20 @@
 import unittest
+import json
 from crawler import Crawler
 
 class CrawlerTest(unittest.TestCase):
 
     def setUp(self):
         self.crawler = Crawler()
+        self.test_data_01 = '{"https://www.crawler-test.com/": {"from": "https://www.crawler-test.com/links/broken_links_internal", "to": "https://www.crawler-test.com/", "is_external": false, "status": 0}, "https://www.crawler-test.com/links/not_found/foo1": {"from": "https://www.crawler-test.com/links/broken_links_internal", "to": "https://www.crawler-test.com/links/not_found/foo1", "is_external": false, "status": 0}, "https://www.crawler-test.com/links/not_found/foo2": {"from": "https://www.crawler-test.com/links/broken_links_internal", "to": "https://www.crawler-test.com/links/not_found/foo2", "is_external": false, "status": 0}, "https://www.crawler-test.com/links/not_found/foo3": {"from": "https://www.crawler-test.com/links/broken_links_internal", "to": "https://www.crawler-test.com/links/not_found/foo3", "is_external": false, "status": 0}, "https://www.crawler-test.com/links/not_found/foo4": {"from": "https://www.crawler-test.com/links/broken_links_internal", "to": "https://www.crawler-test.com/links/not_found/foo4", "is_external": false, "status": 0}, "https://www.crawler-test.com/links/not_found/foo5": {"from": "https://www.crawler-test.com/links/broken_links_internal", "to": "https://www.crawler-test.com/links/not_found/foo5", "is_external": false, "status": 0}}'
 
     def test_find_all_links(self):
         test_url = 'https://www.crawler-test.com/links/broken_links_internal'
         self.crawler.set_start_url(test_url)
         self.crawler.run()
         res = self.crawler.get_results()
-        print(res)
-
-        self.assertEqual('foo'.lower(), 'FOO')
+        print(json.dumps(res))
+        self.assertDictEqual(res, json.loads(self.test_data_01))
 
 
 if __name__ == '__main__':
