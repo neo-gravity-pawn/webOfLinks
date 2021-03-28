a33a8f9e6e34ecc98461be57c6d59481cb958bc5
Neo Gravity <neo_gravity_pawn@outlook.com>
2021-03-28 19:59:10 +0200

    Add recursive url parsing, extend logging, test on real page

--- crawler.py
+++ crawler.py
@@ -1,34 +1,73 @@
 from bs4 import BeautifulSoup
 from urllib import request, parse
+import urllib.error
 
 class Crawler:
     def __init__(self):
         self.results = dict()
+        self.max_nr_iterations = -1
+        self.nr_interations = 0
+        self.domain = ''
 
-    def set_start_url(self, url):
-        self.start_url = url
+    def set_max_nr_of_interations(self, max_nr):
+        self.max_nr_iterations = max_nr
     
-    def run(self):
-        response = request.urlopen(self.start_url)
-        self.__parse_for_links(self.start_url, response)
+    def run(self, url):
+        parsed_url = parse.urlparse(url)
+        self.domain = f"{parsed_url.scheme}://{parsed_url.netloc}"
+        self.__parse_url(url, False)
 
     def get_results(self):
         return self.results
     
+    def __parse_url(self, url, is_external):
+        status = 0
+        print("Parse: ", url)
+        try:
+            response = request.urlopen(url)
+        except urllib.error.HTTPError:
+            status = 404
+        status = response.status if status == 0 else status
+        self.__set_status(url, status) 
+        if not is_external and status == 200:
+            url_links = self.__parse_for_links(url, response)
+            for link in url_links:
+                self.nr_interations += 1
+                if self.max_nr_iterations > 0 and self.nr_interations > self.max_nr_iterations:
+                    break
+                if not self.results[link["url"]]["visited"]:
+                    self.__parse_url(link["url"], link["is_external"])
+    
+    def __set_status(self, url, status):
+        if url in self.results:
+            self.results[url]["status"] = status
+            self.results[url]["visited"] = True
+
     def __parse_for_links(self, url, response):
-        soup = BeautifulSoup(response, 'html.parser')
-        status = response.status
+        links_for_current_url = []
+        soup = BeautifulSoup(response, 'lxml', from_encoding=response.info().get_param('charset'))
         for link in soup.find_all('a', href=True):
-            self.__process_link(url, link['href'])
+            processed_url, is_external = self.__process_link(url, link['href'])
+            if processed_url is not None:
+                links_for_current_url.append({"url": processed_url, "is_external": is_external})
+        return links_for_current_url
 
     def __process_link(self, base_url, url):
-        is_external = url.startswith('http')
-        if not is_external:
-            url = self.__convert_to_absolute_url(base_url, url)
-        already_discoverd = url in self.results
-        if not already_discoverd:
-            self.results[url] = {"from": base_url, "to": url, "is_external": is_external, "status": 0}
-        return self.results[url]
+        if not self.__in_blocklist(url):
+            is_external = not url.startswith(self.domain) and url.startswith('http')
+            if not is_external:
+                url = self.__convert_to_absolute_url(url)
+            if not url in self.results:
+                self.results[url] = {"from": base_url, "to": url, "is_external": is_external, "status": 0, "visited": False}
+                return url, is_external
+        return None, None
     
-    def __convert_to_absolute_url(self, base_url, url):
-        return parse.urljoin(base_url, url) if not url.startswith(base_url) else url
+    def __in_blocklist(self, url):
+        blocklist = ['mailto']
+        for term in blocklist:
+            if url.startswith(term):
+                return True
+        return False
+
+    def __convert_to_absolute_url(self, url):
+        return parse.urljoin(self.domain, url) if not url.startswith('http') else url
--- crawler_test.py
+++ crawler_test.py
@@ -6,16 +6,15 @@ class CrawlerTest(unittest.TestCase):
 
     def setUp(self):
         self.crawler = Crawler()
-        self.test_data_01 = '{"https://www.crawler-test.com/": {"from": "https://www.crawler-test.com/links/broken_links_internal", "to": "https://www.crawler-test.com/", "is_external": false, "status": 0}, "https://www.crawler-test.com/links/not_found/foo1": {"from": "https://www.crawler-test.com/links/broken_links_internal", "to": "https://www.crawler-test.com/links/not_found/foo1", "is_external": false, "status": 0}, "https://www.crawler-test.com/links/not_found/foo2": {"from": "https://www.crawler-test.com/links/broken_links_internal", "to": "https://www.crawler-test.com/links/not_found/foo2", "is_external": false, "status": 0}, "https://www.crawler-test.com/links/not_found/foo3": {"from": "https://www.crawler-test.com/links/broken_links_internal", "to": "https://www.crawler-test.com/links/not_found/foo3", "is_external": false, "status": 0}, "https://www.crawler-test.com/links/not_found/foo4": {"from": "https://www.crawler-test.com/links/broken_links_internal", "to": "https://www.crawler-test.com/links/not_found/foo4", "is_external": false, "status": 0}, "https://www.crawler-test.com/links/not_found/foo5": {"from": "https://www.crawler-test.com/links/broken_links_internal", "to": "https://www.crawler-test.com/links/not_found/foo5", "is_external": false, "status": 0}}'
+        self.test_url_01 = "https://www.dornheim-medical-images.de/"
+        self.test_results_01 = "result_01.json"
 
     def test_find_all_links(self):
-        test_url = 'https://www.crawler-test.com/links/broken_links_internal'
-        self.crawler.set_start_url(test_url)
-        self.crawler.run()
-        res = self.crawler.get_results()
-        print(json.dumps(res))
-        self.assertDictEqual(res, json.loads(self.test_data_01))
+        self.crawler.run(self.test_url_01)
+        result = self.crawler.get_results()
+        with open(self.test_results_01, "r") as file:
+            test_result_01 = json.load(file)
+        self.assertDictEqual(result, test_result_01)
 
-
-if __name__ == '__main__':
+if __name__ == "__main__":
     unittest.main()
